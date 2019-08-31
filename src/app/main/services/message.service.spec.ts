bab45e72546b26527b3051c99d8e4128e9dd99bc
jpart-r <jpartdev@gmail.com>
2019-08-31 16:32:14 +0800

    Fix test

--- src/app/main/services/message.service.spec.ts
+++ src/app/main/services/message.service.spec.ts
@@ -1,9 +1,16 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { MessageService } from './message.service';
 
 describe('MessageService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [
+      HttpClientTestingModule
+    ],
+    providers: [
+      MessageService
+    ]
+  }));
 
   it('should be created', () => {
     const service: MessageService = TestBed.get(MessageService);
